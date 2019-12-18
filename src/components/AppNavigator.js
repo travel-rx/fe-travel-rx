a405c8da84ddda773c6e016e2e0cf18c2500b83f
Amy Rippeto <amyrippeto@gmail.com>
2019-12-18 14:13:30 -0700

    Setup navigator

---
+++ src/components/AppNavigator.js
@@ -0,0 +1,8 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import Home from '../components/Home/Home';
+
+const AppNavigator = createStackNavigator({
+  Home: { screen: Home }
+});
+
+export default AppNavigator;
