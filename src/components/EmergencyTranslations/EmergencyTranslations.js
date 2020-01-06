f14feee6bcad7d25ed6d48df6b501f267308230e
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-01-06 15:58:35 -0700

    Render phrases based on hard coded language

--- src/components/EmergencyTranslations/EmergencyTranslations.js
+++ src/components/EmergencyTranslations/EmergencyTranslations.js
@@ -20,12 +20,22 @@ export default class EmergencyTranslations extends Component {
 
   render() {
     const { navigation } = this.props;
+    const language = data['french']
     console.log('what is DATA?', data.spanish)
 
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.tripsContainer}>
-          <Text>test</Text>
+        <ScrollView style={styles.translationContainer}>
+          <Text>Help</Text>
+          <Text>{language.help}</Text>
+          <Text>I need a doctor</Text>
+          <Text>{language.doctor}</Text>
+          <Text>There's been an accident</Text>
+          <Text>{language.accident}</Text>
+          <Text>I need an ambulance</Text>
+          <Text>{language.ambulance}</Text>
+          <Text>Medicine</Text>
+          <Text>{language.medication}</Text>
         </ScrollView>
         <Footer navigation={navigation}/>
       </View>
@@ -43,7 +53,6 @@ const styles = StyleSheet.create({
     color: '#3499AA',
     fontSize: 40,
     paddingTop: 20,
-
   },
   buttonText: {
     color: '#EBEBEB',
@@ -51,26 +60,14 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-  tripsContainer: {
+  translationContainer: {
     margin: 25,
     marginHorizontal: 15
-
   },
   trip: {
     flex: 1,
     flexDirection: 'row',
     marginBottom: 20
-  },
-  tripName: {
-    color: '#3499AA',
-    fontSize: 25,
-    fontWeight: 'bold',
-    marginLeft: 20
-  },
-  tripDestination: {
-    color: '#212B31',
-    fontSize: 15,
-    marginLeft: 20
   },
   button: {
     backgroundColor: '#3499AA',
