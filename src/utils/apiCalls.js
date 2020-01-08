3a0f53ada163fcbb6d0d04ca07433ad8dfcb7794
Amy Rippeto <amyrippeto@gmail.com>
2020-01-08 14:21:43 -0700

    Update methods for delete and update translations

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,12 +1,10 @@
-import data from './data';
-
- export const getDrug = async (drug) => {
-   const response = await fetch(`https://flask-travel-rx.herokuapp.com/api/v1/search?drug=${drug}`)
-   if (!response.ok && response.status >= 500) {
+export const getDrug = async (drug) => {
+  const response = await fetch(`https://flask-travel-rx.herokuapp.com/api/v1/search?drug=${drug}`)
+  if (!response.ok && response.status >= 500) {
      throw Error('Unable to find this medication. Please check the spelling and try again.')
-   } else if (!response.ok && response.status < 500) {
+  } else if (!response.ok && response.status < 500) {
      throw Error('Looks like a problem with the network. Please try again later.')
-   }
+  }
   const names = await response.json();
   return names.generic_name
 }
@@ -47,16 +45,14 @@ export const deleteMed = async (id) => {
   const options = {
     method: 'DELETE',
     header: {
-      'Content-Type': 'application/json'
+      'content-type': 'application/json'
     }
   }
   const response = await fetch(`https://flask-travel-rx.herokuapp.com/api/v1/user/1/medicines/${id}`, options)
-  console.log('here', response.text())
-  // if (!response.ok) {
-  //   throw Error('Unable to delete your medication. Please try again later.')
-  // }
+  if (!response.ok) {
+    throw Error('Unable to delete your medication. Please try again later.')
+  }
   const cleanedMeds = await response.json();
   return cleanedMeds;
-  // return data.meds.filter(med => med.id != id)
 
 }
