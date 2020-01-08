73bf5f319517665c694bd81005a167a6c7958398
Amy Rippeto <amyrippeto@gmail.com>
2020-01-08 16:24:37 -0700

    Add test for postMed

--- src/utils/apiCalls.test.js
+++ src/utils/apiCalls.test.js
@@ -58,12 +58,12 @@ describe('getMeds', () => {
     })
   });
 
-  it('should call getDrug with the correct url', () => {
+  it('should call getMeds with the correct url', () => {
     getMeds()
     expect(window.fetch).toHaveBeenCalledWith('https://flask-travel-rx.herokuapp.com/api/v1/user/1/medicines');
   });
 
-  it('should return an object with the drug name and generic name', () => {
+  it('should return an array of meds', () => {
     expect(getMeds()).resolves.toEqual(mockMeds);
   })
 
@@ -83,6 +83,80 @@ describe('getMeds', () => {
     });
 
     expect(getMeds()).rejects.toEqual(Error('fetch failed'));
+  });
+
+})
+
+describe('postMed', () => {
+  const med = {
+    name: 'Xanax',
+    generic_Name: 'ALPRAZOLAM',
+    dosage: '20mg',
+    frequency: 2,
+    food: false,
+    userId: 1
+  }
+  
+  const newMed = {
+    id: 1,
+    name: 'Xanax', 
+    generic_name: 'ALPRAZOLAM',
+    dosage_amt: '20mg',
+    frequency: 2,
+    with_food: false,
+    user_id: 1
+  }
+
+  const mockResponse = [...mockMeds, newMed];
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      })
+    })
+  });
+
+  it('should call postMed with the correct url and options', () => {
+    postMed(med)
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        name: med.name,
+        generic_name: med.genericName,
+        dosage_amt: med.dosage,
+        frequency: parseInt(med.frequency),
+        with_food: med.food,
+        user_id: med.userId
+      }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }
+    expect(window.fetch).toHaveBeenCalledWith('https://flask-travel-rx.herokuapp.com/api/v1/user/1/medicines', options);
+  });
+
+  it('should return an array of meds including the added med', () => {
+    expect(postMed()).resolves.toEqual(mockResponse);
+  })
+
+  it('should throw and error if the resonse is', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      })
+    });
+
+    expect(postMed()).rejects.toEqual(Error('Unable to add medication at this time. Please try again later.'));
+  });
+
+  it('should throw an error if the server is down', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error('fetch failed.'))
+    });
+
+    expect(postMed()).rejects.toEqual(Error('fetch failed'));
   });
 
 })
